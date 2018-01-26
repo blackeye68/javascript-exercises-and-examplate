2cbf967b041843c049a8b6f365010c78776e8da6
tu.cuong.bat.tuc@gmail.com <24867193@vohoi>
2018-01-26 23:51:38 +0700

    jajaja

---
+++ 4 function javascript co ban cho frontedn - vi du 2/1.js
@@ -0,0 +1,20 @@
+﻿// demo template string
+var mangdl = [
+{
+	anh:"http://kenh14cdn.com/zoom/280_175/2018/photo1516094796401-1516094796401.jpg",
+	ten:"quai vat",
+	noidungcm:"Những bộ phim hoạt hình tốn kém hàng triệu USD bây giờ có thể quay bằng camera iPhone X"
+},
+
+{
+	anh:"http://kenh14cdn.com/zoom/280_175/2018/photo1516117537814-1516117537815.jpg",
+	ten:"mu dep",
+	noidungcm:"Kỳ Duyên khoe loạt hàng hiệu Gucci mới sắm, đắt nhất là chiếc áo khoác hơn 50 triệu"
+}
+
+];
+// console.log(mangdl[0]['noidungcm']);
+
+for (var i = 0; i < mangdl.length; i++) {
+	console.log(mangdl[i].ten);
+}
---
+++ 4 function javascript co ban cho frontend - vi du 1/1.js
@@ -0,0 +1,13 @@
+﻿// demo hàm tạo và dính phần tử trong javascript
+
+// tạo 1 thẻ mới
+var phantu = document.createElement('h1');
+
+// gắn nội dung cho thẻ đó
+phantu.innerHTML = "Đây là nội dung bên trong "; //đến đây là mình có thẻ <h1>Đây là nội dung bên trong</h1>
+
+// lấy phần tử cần đặt vào qua id của phần tử 
+var khoito = document.getElementById('caccm');
+
+// gắn nội dung vào web
+khoito.appendChild(phantu);
