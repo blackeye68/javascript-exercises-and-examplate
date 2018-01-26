9f220638d11dd1bb07372c8459346be66dc3ba12
tu.cuong.bat.tuc@gmail.com <24867193@vohoi>
2018-01-26 23:48:33 +0700

    update 1

---
+++ vi du - hieu ung animation bang javascript/1.js
@@ -0,0 +1,18 @@
+﻿// truy xuất đến phần tử
+
+
+// trước khi truy xuất phải document ready
+document.addEventListener("DOMContentLoaded", function(){
+	// dùng code
+	var nut = document.getElementById('nut1');
+
+	var khoi = document.getElementsByClassName('card');
+	// console.log(khoi);
+	// gọi sự kiện click
+	nut.onclick = function() {
+		// console.log('click được chưa ?');
+		// khoi[0].classList.add('diphai');
+		khoi[0].classList.toggle('diphai');
+	}
+
+}, false)
